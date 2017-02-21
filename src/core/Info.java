61f32ac38b9c330c44d83190fcf82216eb367eb3
Anne1231 <at.sisy@gmail.com>
2017-02-21 20:22:53 +0900

    このライブラリの情報を吐く関数を定義

---
+++ src/core/Info.java
@@ -0,0 +1,11 @@
+package core;
+
+/**
+ * Created by Anne on 2017/02/21.
+ */
+public class Info {
+    public static void PrintInfo(){
+        System.out.println("yuri cvfx version 0.1.0");
+        System.out.println("February 21 2017");
+    }
+}
